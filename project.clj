1d60d5415b73aa5708fc02b007de433d2a69d8f4
JeongHwan-Song <jhsong@beyondmedicine.kr>
2023-11-20 13:37:12 +0900

    [시스템 최적화] 예제 풀이

--- project.clj
+++ project.clj
@@ -3,5 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [
+                 [org.clojure/clojure "1.11.1"]
+                 [org.apache.commons/commons-math3 "3.2"]
+                 [net.mikera/core.matrix "0.63.0"]
+                 ]
   :repl-options {:init-ns clj.core})
